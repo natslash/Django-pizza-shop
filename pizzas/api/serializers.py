4a1a53ed6de87222ec87bb65199e3fd7446ce224
Enrique Cases Sánchez <ecases@red.local>
2017-05-11 11:37:38 +0200

    Updated POST

--- pizzas/api/serializers.py
+++ pizzas/api/serializers.py
@@ -18,37 +18,17 @@ class PizzaSerializer(serializers.ModelSerializer):
         model = Pizza
         fields = "__all__"
 
-#    def create(self, validated_data):
-#        user_data = validated_data.pop('user')
-#        user = User.objects.create(**user_data)
-#        user.set_password(user_data['password'])
-#        user.save()
-#        consumer = Consumer.objects.create(user=user, **validated_data)
-#        return consumer
-
-#    def create(self, request, *args, **kwargs):
-#        serializer = self.get_serializer(data=request.data)
-#        serializer.is_valid(raise_exception=True)
-#        self.perform_create(serializer)
-#        headers = self.get_success_headers(serializer.data)
-#        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-    def create(self, **kwargs):
-        """
-        Creates a new object with the given kwargs, saving it to the database
-        and returning the created object.
-        """
-        obj = self.model(**kwargs)
-        self._for_write = True
-        obj.save(force_insert=True, using=self.db) # calls the `save()` method here
-        return obj
-
-#    def create(self, validated_data):
-#        user_data = validated_data.pop('user')
-#        user = User.objects.create(**validated_data)
-#        Profile.objects.create(user=user, **user_data)
-#        return user
-
+    def create(self, validated_data):
+        array = []
+        ingredients_data = validated_data.pop('ingredients');
+        for ingredientes in ingredients_data:
+            p = Ingredient.objects.create(**ingredientes)
+            array.append(p)
+        pizza = Pizza.objects.create(**validated_data)
+        #pizza.ingredients.add(array[0])
+        for i in range(len(array)):
+            pizza.ingredients.add(array[i])
+        return pizza
 
 
 class CommentSerializer(serializers.ModelSerializer):
