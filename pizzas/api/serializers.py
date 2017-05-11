267818521fcda663cef3021cdb38ef296ddb426c
Enrique Cases Sánchez <ecases@red.local>
2017-05-11 13:38:51 +0200

    New updated POST

--- pizzas/api/serializers.py
+++ pizzas/api/serializers.py
@@ -14,19 +14,28 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 class PizzaSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(many=True)
+    print(ingredients)
     class Meta:
         model = Pizza
         fields = "__all__"
 
     def create(self, validated_data):
         array = []
-        ingredients_data = validated_data.pop('ingredients');
-        for ingredientes in ingredients_data:
-            p = Ingredient.objects.create(**ingredientes)
+        dict={}
+        
+        ingredients_data = validated_data.pop('ingredients')
+        print(ingredients_data)
+        for ingredient in ingredients_data:
+            print("#########",ingredient["name"])
+            p = Ingredient.objects.filter(name=ingredient["name"])[0]
+            ##probar con get
+            #print("%%%%%%%%%",p)
             array.append(p)
+            
         pizza = Pizza.objects.create(**validated_data)
         #pizza.ingredients.add(array[0])
         for i in range(len(array)):
+            #print("--------",array[i])
             pizza.ingredients.add(array[i])
         return pizza
 
