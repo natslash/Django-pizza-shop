4c08a2e9c6f47a844524a22cde74ce54bf6d0943
Isidro Torregrosa Torralba <isistorre95@gmail.com>
2017-05-13 15:52:15 +0200

    test

--- static/app/pizza/listall/pizzalistservice.js
+++ static/app/pizza/listall/pizzalistservice.js
@@ -1,14 +1,14 @@
 import { App } from '../../module';
 
-pizzaListService.$inject = ['$http', 'resolveUrl']
-function pizzaListService($http, resolveUrl) {
+pizzaListService.$inject = ['$http']
+function pizzaListService($http) {
     return {
         getPizzas: function () {
             return $http.get(
                 // 'http://localhost:3000/pizzas'
                 // 'http://localhost:8000/api/v1/pizzas'
-                resolveUrl.resolve('pizzas')
-                // 'http://demo-pizzashop.herokuapp.com/api/v1/pizzas'
+                // resolveUrl.resolve('pizzas')
+                'http://demo-pizzashop.herokuapp.com/api/v1/pizzas'
             );
         }
     };
