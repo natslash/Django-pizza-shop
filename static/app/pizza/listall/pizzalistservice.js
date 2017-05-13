bb34bb45776cdce6959c96d607a375db74024564
Isidro Torregrosa Torralba <isistorre95@gmail.com>
2017-05-13 15:58:38 +0200

    deployed

--- static/app/pizza/listall/pizzalistservice.js
+++ static/app/pizza/listall/pizzalistservice.js
@@ -5,10 +5,7 @@ function pizzaListService($http) {
     return {
         getPizzas: function () {
             return $http.get(
-                // 'http://localhost:3000/pizzas'
-                // 'http://localhost:8000/api/v1/pizzas'
-                // resolveUrl.resolve('pizzas')
-                'http://demo-pizzashop.herokuapp.com/api/v1/pizzas'
+                '/api/v1/pizzas'
             );
         }
     };
